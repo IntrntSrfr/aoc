cf56a8b04586d4a3d5237f002d22badc989a28cb
IntrntSrfr <aleksander@tosbakken.com>
2020-12-01 15:01:14 +0100

    first

---
+++ day-1/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
+
+func main() {
+	inp := getInputs()
+
+	sort.Ints(inp)
+
+	fmt.Println("part 1:")
+	part1(inp)
+	fmt.Println("part 2:")
+	part2(inp)
+}
+
+func part1(inp []int) {
+	start := time.Now()
+	found := false
+	for i := 0; i < len(inp); i++ {
+		for j := i + 1; j < len(inp); j++ {
+			if inp[i]+inp[j] == 2020 {
+				fmt.Println(inp[i], inp[j], inp[i]*inp[j])
+				found = true
+				break
+			}
+		}
+		if found {
+			break
+		}
+	}
+	fmt.Println("time taken:", time.Now().Sub(start))
+}
+
+func part2(inp []int) {
+	start := time.Now()
+	found := false
+	for i := 0; i < len(inp); i++ {
+		for j := i + 1; j < len(inp); j++ {
+			for k := j + 1; k < len(inp); k++ {
+				if inp[i]+inp[j]+inp[k] == 2020 {
+					fmt.Println(inp[i], inp[j], inp[k], inp[i]*inp[j]*inp[k])
+					found = true
+					break
+				}
+			}
+			if found {
+				break
+			}
+		}
+		if found {
+			break
+		}
+	}
+	fmt.Println("time taken:", time.Now().Sub(start).String())
+}
+
+func getInputs() []int {
+	f, _ := os.Open("./input.txt")
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	var res []int
+	for scanner.Scan() {
+		n, _ := strconv.Atoi(scanner.Text())
+		res = append(res, n)
+	}
+
+	return res
+}
