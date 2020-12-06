a2f0467ad412800ff5a6913f5270274b1e6d397b
IntrntSrfr <aleksander@tosbakken.com>
2020-12-06 07:24:36 +0100

    day 5 refactor, day 6

--- day-5/main.go
+++ day-5/main.go
@@ -1,65 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
-	"strconv"
-	"strings"
+	"os"
+	"sort"
 	"time"
 )
 
 func main() {
-	passes := getInputs()
-
 	start := time.Now()
 
+	var IDs []int
+	f, _ := os.Open("./input.txt")
+	defer f.Close()
+	r := bufio.NewReader(f)
+
+	p1 := time.Now()
 	highest := 0
-	var list []int
-	for _, pass := range passes {
-		pass = strings.ReplaceAll(pass, "F", "0")
-		pass = strings.ReplaceAll(pass, "B", "1")
-		pass = strings.ReplaceAll(pass, "L", "0")
-		pass = strings.ReplaceAll(pass, "R", "1")
-
-		row, _ := strconv.ParseInt(pass[:7], 2, 64)
-		seat, _ := strconv.ParseInt(pass[len(pass)-3:], 2, 64)
-
-		sID := int(row*8 + seat)
-		if sID > highest {
-			highest = sID
+	for {
+		pass, _, err := r.ReadLine()
+		if err != nil {
+			break
 		}
-		list = append(list, sID)
-	}
-	fmt.Println(highest)
-
-	for i := 0; i < highest; i++ {
-		if inList(list, i-1) && inList(list, i+1) && !inList(list, i) {
-			fmt.Println(i)
-		}
-	}
-
-	fmt.Println(time.Since(start))
-
-	/*
-		sort.Ints(list)
-		for i := 1; i < len(list)-1; i++ {
-			if list[i+1] != list[i]+1 {
-				fmt.Println(list[i]+1)
+		n := 0
+		for _, b := range pass {
+			if b == byte('F') || b == byte('L') {
+				n = n << 1
+			} else {
+				n = (n << 1) + 1
 			}
 		}
-	*/
-}
+		if n > highest {
+			highest = n
+		}
+		IDs = append(IDs, n)
+	}
+	fmt.Println("Part 1:", highest)
+	fmt.Println("Part 1 time:", time.Since(p1))
+	fmt.Println()
 
-func inList(list []int, n int) bool {
-	for _, l := range list {
-		if l == n {
-			return true
+	p2 := time.Now()
+	sort.Ints(IDs)
+	for i := 0; i < len(IDs)-1; i++ {
+		if IDs[i+1] != IDs[i]+1 {
+			fmt.Println("Part 2:", IDs[i]+1)
+			break
 		}
 	}
-	return false
-}
+	fmt.Println("Part 2 time:", time.Since(p2))
 
-func getInputs() []string {
-	data, _ := ioutil.ReadFile("./input.txt")
-	return strings.Split(string(data), "\n")
+	fmt.Println()
+	fmt.Println("Full time:", time.Since(start))
+	return
 }
