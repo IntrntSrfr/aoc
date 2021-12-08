e76fe0e38ebde1f8d9f55dff7b07c9329e25f1d9
IntrntSrfr <aleksander@tosbakken.com>
2021-12-08 19:28:24 +0100

    8

--- 2021/8/main.go
+++ 2021/8/main.go
@@ -5,14 +5,19 @@ import (
 	"fmt"
 	"math/bits"
 	"os"
-	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
 	inp := getInputs()
+	p1Start := time.Now()
 	part1(inp)
+	fmt.Println("part 1 time:  ", time.Since(p1Start))
+	p2Start := time.Now()
 	part2(inp)
+	fmt.Println("part 2 time:  ", time.Since(p2Start))
+	fmt.Println("total time:   ", time.Since(p1Start))
 }
 
 func part1(inp [][]uint) {
@@ -25,11 +30,10 @@ func part1(inp [][]uint) {
 			}
 		}
 	}
-	fmt.Println(count)
+	fmt.Println("part 1 answer:", count)
 }
 
 func part2(inp [][]uint) {
-
 	sum := 0
 	for _, line := range inp {
 		one, seven, four, eight := cwbc(line[:10], 2)[0], cwbc(line[:10], 3)[0], cwbc(line[:10], 4)[0], cwbc(line[:10], 7)[0]
@@ -51,27 +55,28 @@ func part2(inp [][]uint) {
 		c := eight & (0b1111111 ^ (transfer | e | d))
 		f := eight & (0b1111111 ^ (a | b | c | d | e | g))
 
-		mapping := map[uint]string{
-			a | b | c | e | f | g:     "0",
-			c | f:                     "1",
-			a | c | d | e | g:         "2",
-			a | c | d | f | g:         "3",
-			b | c | d | f:             "4",
-			a | b | d | f | g:         "5",
-			a | b | d | e | f | g:     "6",
-			a | c | f:                 "7",
-			a | b | c | d | e | f | g: "8",
-			a | b | c | d | f | g:     "9",
+		mapping := map[uint]int{
+			a | b | c | e | f | g:     0,
+			c | f:                     1,
+			a | c | d | e | g:         2,
+			a | c | d | f | g:         3,
+			b | c | d | f:             4,
+			a | b | d | f | g:         5,
+			a | b | d | e | f | g:     6,
+			a | c | f:                 7,
+			a | b | c | d | e | f | g: 8,
+			a | b | c | d | f | g:     9,
 		}
 
-		str := ""
+		base := 1000
+		num := 0
 		for _, ye := range line[10:] {
-			str += mapping[ye]
+			num += mapping[ye] * base
+			base /= 10
 		}
-		num, _ := strconv.Atoi(str)
 		sum += num
 	}
-	fmt.Println(sum)
+	fmt.Println("part 2 answer:", sum)
 }
 
 func cwbc(a []uint, count int) []uint {
@@ -108,21 +113,16 @@ func codeToInt(code string) uint {
 }
 
 func getInputs() [][]uint {
-	f, _ := os.Open("./input.txt")
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-
+	scanner := bufio.NewScanner(os.Stdin)
 	var lines [][]uint
 	for scanner.Scan() {
 		line := scanner.Text()
 		line = strings.ReplaceAll(line, "|", "")
 		fields := strings.Fields(line)
-
 		var ints []uint
 		for _, numStr := range fields {
 			ints = append(ints, codeToInt(numStr))
 		}
-
 		lines = append(lines, ints)
 	}
 	return lines
