cc3f993a106f3a74d7cf0401964792e497c1f600
IntrntSrfr <aleksander@tosbakken.com>
2021-12-10 09:06:49 +0100

    6 return to puzzle from big boy

--- 2021/6/main.go
+++ 2021/6/main.go
@@ -7,39 +7,21 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 )
 
 func main() {
 	state := getInputs()
-
-	// 9999999
-
-	start := time.Now()
-	iters := 10
-	for i := 0; i < iters; i++ {
-		solve(deepCopy(state), 256)
-	}
-
-	fmt.Println(fmt.Sprintf("done in %v total; %v average", time.Since(start), time.Since(start)/time.Duration(iters)))
+	solve(state, 256)
 }
 
 func solve(state []*big.Int, days int) {
 	for day := 0; day < days; day++ {
 		state[(day+7)%9].Add(state[(day+7)%9], state[day%9])
-		//if day == 79 {
-		//	fmt.Println(sum(state))
-		//}
+		if day == 79 {
+			fmt.Println(sum(state))
+		}
 	}
-	//fmt.Println(sum(state))
-}
-
-func deepCopy(s []*big.Int) []*big.Int {
-	res := make([]*big.Int, len(s))
-	for i := range res {
-		res[i] = big.NewInt(0).Set(s[i])
-	}
-	return res
+	fmt.Println(sum(state))
 }
 
 func getInputs() []*big.Int {
@@ -62,4 +44,12 @@ func sum(m []*big.Int) *big.Int {
 		sum.Add(sum, v)
 	}
 	return sum
+}
+
+func deepCopy(s []*big.Int) []*big.Int {
+	res := make([]*big.Int, len(s))
+	for i := range res {
+		res[i] = big.NewInt(0).Set(s[i])
+	}
+	return res
 }
