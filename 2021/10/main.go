b969742e6315323b3a9dc81d50045c109f33e431
IntrntSrfr <aleksander@tosbakken.com>
2021-12-10 13:48:40 +0100

    10

--- 2021/10/main.go
+++ 2021/10/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"sort"
+
+	"github.com/intrntsrfr/aoc/utils"
 )
 
 func main() {
@@ -29,7 +31,7 @@ func main() {
 }
 
 func isValid(s string) (bool, int) {
-	st := NewStack()
+	st := utils.NewStack()
 	for _, c := range s {
 		if c == '{' || c == '(' || c == '[' || c == '<' {
 			st.Push(int(c))
@@ -54,45 +56,3 @@ var p1map = map[rune][]int{
 	'>': {int('<'), 25137},
 }
 var p2map = map[int]int{int('('): 1, int('['): 2, int('{'): 3, int('<'): 4}
-
-type Stack struct {
-	top    *Node
-	length int
-}
-
-func NewStack() *Stack {
-	return &Stack{nil, 0}
-}
-
-func (s *Stack) Empty() bool {
-	return s.length == 0
-}
-
-func (s *Stack) Peek() int {
-	if s.Empty() {
-		return -1
-	}
-	return s.top.value
-}
-
-func (s *Stack) Pop() int {
-	if s.Empty() {
-		return -1
-	}
-
-	n := s.top
-	s.top = n.prev
-	s.length--
-	return n.value
-}
-
-func (s *Stack) Push(value int) {
-	n := &Node{value, s.top}
-	s.top = n
-	s.length++
-}
-
-type Node struct {
-	value int
-	prev  *Node
-}
