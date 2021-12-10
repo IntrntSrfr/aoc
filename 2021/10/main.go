e8736f0f4ea7ee4084fa660a95eb3fa4d5d63bf6
IntrntSrfr <aleksander@tosbakken.com>
2021-12-10 16:51:21 +0100

    10 bigboy

--- 2021/10/main.go
+++ 2021/10/main.go
@@ -3,34 +3,38 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/big"
 	"os"
 	"sort"
+	"time"
 
 	"github.com/intrntsrfr/aoc/utils"
 )
 
 func main() {
-	f, _ := os.Open("./input.txt")
+	f, _ := os.Open("./10_7000_10000.txt")
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 
-	p1Score := 0
-	p2Scores := []int{}
+	start := time.Now()
+	p1Score := big.NewInt(0)
+	p2Scores := []*big.Int{}
 	for scanner.Scan() {
 		line := scanner.Text()
 		valid, s := isValid(line)
 		if !valid {
-			p1Score += s
+			p1Score = p1Score.Add(p1Score, s)
 		} else {
 			p2Scores = append(p2Scores, s)
 		}
 	}
-	sort.Ints(p2Scores)
 	fmt.Println("part 1 answer:", p1Score)
+	sort.Sort(utils.BigIntSlice(p2Scores))
 	fmt.Println("part 2 answer:", p2Scores[len(p2Scores)/2])
+	fmt.Println("time taken:", time.Since(start))
 }
 
-func isValid(s string) (bool, int) {
+func isValid(s string) (bool, *big.Int) {
 	st := utils.NewStack()
 	for _, c := range s {
 		if c == '{' || c == '(' || c == '[' || c == '<' {
@@ -38,13 +42,14 @@ func isValid(s string) (bool, int) {
 			continue
 		}
 		if st.Peek() != p1map[c][0] {
-			return false, p1map[c][1]
+			return false, big.NewInt(int64(p1map[c][1]))
 		}
 		st.Pop()
 	}
-	score := 0
+	score := big.NewInt(0)
 	for !st.Empty() {
-		score = score*5 + p2map[st.Pop()]
+		score = score.Mul(score, big.NewInt(5))
+		score = score.Add(score, big.NewInt(int64(p2map[st.Pop()])))
 	}
 	return true, score
 }
