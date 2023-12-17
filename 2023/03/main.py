5ecc018917a91bd6786a6971cd82d1e0c0bac422
IntrntSrfr <aleksander@tosbakken.com>
2023-12-17 23:57:32 +0100

    day 3

--- 2023/03/main.py
+++ 2023/03/main.py
@@ -1,41 +1,53 @@
-from itertools import permutations
+import re
 
 inp = [line.strip() for line in open('inp').readlines()]
+blocks = []
+for y, line in enumerate(inp):
+    blocks.extend([
+        (list((y, x) for x in range(m.start(), m.end())), int(m.group())) 
+        for m in re.finditer(r'\d+', line)
+    ])
 
-blocks = [] # ([(i, j1), (i, j2), (i, j3)], number)
-for i, line in enumerate(inp):
-    current_int_str = ''
-    current_int_positions = []
-    for j, char in enumerate(line):
-        if char.isdigit():
-            current_int_str += char
-            current_int_positions.append((i, j))
-        else:
-            if current_int_str == '':
-                continue
-            blocks.append((current_int_positions, int(current_int_str)))
-            current_int_str = ''
-            current_int_positions = []
+gears = []
+for y, line in enumerate(inp):
+    gears.extend([(y, m.start()) for m in re.finditer(r'\*', line)])
 
-p1_score = 0
-for block in blocks:
-    is_part = False
-    for coords in block[0]:
-        for i in range(-1, 2):
-            for j in range(-1, 2):
-                if j == 0 and i == 0:
+def p2():
+    score = 0
+    seen = set()
+    for (y, x) in gears:
+        parts = []
+        for dy in range(-1, 2):
+            for dx in range(-1, 2):
+                if dy == 0 and dx == 0:
                     continue
-                check_y = coords[0]+i
-                check_x = coords[1]+j
-                if ((check_y, check_x) in coords or 
-                    check_y < 0 or check_y > len(inp)-1 or 
-                    check_x < 0 or check_x > len(inp[0])-1): 
+                if (y+dy, x+dx) in seen:
                     continue
+                for block in blocks:
+                    if (y+dy, x+dx) in block[0]:
+                        parts.append(block[1])
+                        seen.update(block[0])
+        if len(parts) == 2:
+            score += parts[0]*parts[1]
+    return score
 
-                char = inp[check_y][check_x]
-                if char in '.1234567890' :
-                    continue
-                is_part = True
-    if is_part:
-        p1_score += block[1]
-print(p1_score)
+def p1():
+    score = 0
+    for block in blocks:
+        coords, num = block
+        is_part = False
+        for (y, x) in coords:
+            for dy in range(-1, 2):
+                for dx in range(-1, 2):
+                    if dy == 0 and dx == 0:
+                        continue
+                    if y + dy < 0 or y+dy >= len(inp) or x+dx <0 or x+dx >= len(inp[0]):
+                        continue
+                    if inp[y+dy][x+dx] in '.1234567890':
+                        continue
+                    is_part = True
+        if is_part:
+            score += num
+    return score
+
+print(p1(), p2())
--- 2023/03/main2.py
+++
@@ -1,7 +0,0 @@
-inp = [line.strip() for line in open('inp').readlines()]
-
-for line in inp:
-    current_int_str = ''
-    q = []
-    for i in range(len(inp)):
-        print(i)
