de584c7ef489e201a728a03bbb02f0a4b0a65c00
IntrntSrfr <aleksander@tosbakken.com>
2023-12-28 03:47:59 +0100

    d5

--- 2023/05/main.py
+++ 2023/05/main.py
@@ -1,11 +1,3 @@
-inp = open("inp").read().split("\n\n")
-seeds = list(map(int, inp.pop(0).split(" ")[1:]))
-maps = []
-for m in inp:
-    parts = m.split()[2:]
-    maps.append([list(map(int, parts[i : i + 3])) for i in range(0, len(parts), 3)])
-
-
 def p1(seeds, maps):
     for map in maps:
         next_seeds = []
@@ -21,40 +13,45 @@ def p1(seeds, maps):
         seeds = next_seeds
     return min(seeds)
 
+def split(r1, r2):
+    a, b = r1
+    c, d = r2
 
-def split(r1: range, r2: range):
-    overlap_start = max(r1.start, r2.start)
-    overlap_end = min(r1.stop, r2.stop)
-
-    # Splitting into three parts
-    before = range(r1.start, overlap_start) if overlap_start > r1.start else None
-    overlap = range(overlap_start, overlap_end) if overlap_start < overlap_end else None
-    after = range(overlap_end, r1.stop) if overlap_end < r1.stop else None
-
-    return before, overlap, after
-
-
-def combine(r1: range, r2: range):
-    return (
-        (min(r1.start, r2.start), max(r1.stop, r2.stop))
-        if r1.start <= r2.stop and r2.start <= r1.stop
-        else None
-    )
-
+    if max(a, c) < min(b, d):
+        before = (a, max(a, c))
+        overlap = (max(a, c), min(b, d))
+        after = (min(b, d), b)
+        before = before if before[0] != before[1] else ()
+        after = after if after[0] != after[1] else ()
+        return [before, overlap, after]
+    return [r1]
 
 def p2(seeds, maps):
-    seeds = [range(seeds[x], seeds[x] + seeds[x + 1]) for x in range(0, len(seeds), 2)]
-    
+    seeds = [(seeds[x], seeds[x] + seeds[x + 1]) for x in range(0, len(seeds), 2)]
     for map in maps:
-        next_seeds = []
+        next_seeds = set()
         for seed in seeds:
+            split_seeds = set()
+            mapped = False
             for dst, src, r in map:
-                print(seed, dst, src, r)
-                lol = split(range(src, src+r), seed)
-                print(lol)
-        seeds = next_seeds
-    print(seeds)
+                splits = split(seed, (src, src+r))
+                if len(splits) == 3:
+                    mapped = True
+                    seeds.extend([x for x  in [splits[0], splits[2]] if len(x)])
+                    next_seeds.add((splits[1][0]+(dst - src), splits[1][1]+(dst - src)))
+                    continue
+                else:
+                    split_seeds.update(splits)
+            if not mapped:
+                next_seeds.update(split_seeds)
+        seeds = list(next_seeds)
+    return min(seeds)[0]
 
+inp = open("inp").read().split("\n\n")
+seeds = list(map(int, inp.pop(0).split(" ")[1:]))
+maps = []
+for m in inp:
+    parts = m.split()[2:]
+    maps.append([list(map(int, parts[i : i + 3])) for i in range(0, len(parts), 3)])
 
-print(p1(seeds, maps))
-p2(seeds, maps)
+print(p1(seeds, maps), p2(seeds, maps))
