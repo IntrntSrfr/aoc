e883562c7f4db5bda342eab1d9046ba71cac64f5
IntrntSrfr <aleksander@tosbakken.com>
2023-12-31 05:45:59 +0100

    d13

--- 2023/13/main.py
+++ 2023/13/main.py
@@ -1,78 +1,62 @@
-#inp = [y for x in open('inp').read().split('\n\n') for y in x.split()]
-inp = [[[char for char in row] for row in layer.split('\n')] for layer in open('inp').read().split('\n\n')]
-#print(inp)
+def transpose(l):
+    return ["".join(r) for r in zip(*l)]
 
-def transpose(l: list[str]):
-    return [''.join(r) for r in zip(*l)]
-
-def find_mirror_idx(grid):
+def find_mirror_idx(mirr):
     mirror_idxs = []
-    for i in range(len(grid)-1):
-        if grid[i] == grid[i+1]:
+    for i in range(len(mirr) - 1):
+        if mirr[i] == mirr[i + 1]:
             mirror_idxs.append(i)
-
-    ret = 0
-    for mirror_idx in mirror_idxs:
-        for i in range(0, mirror_idx+1):
-            if (mirror_idx-i == 0 or mirror_idx + i + 1 == len(grid)-1) and grid[mirror_idx-i] == grid[mirror_idx+i+1]:
-                ret = mirror_idx + 1
+    rets = []
+    for idx in mirror_idxs:
+        for i in range(0, idx + 1):
+            if (
+                (idx - i == 0 or idx + i + 1 == len(mirr) - 1) and 
+                mirr[idx - i] == mirr[idx + i + 1]
+            ):
+                rets.append(idx + 1)
                 break
-            if grid[mirror_idx-i] != grid[mirror_idx+i+1]:
+            if mirr[idx - i] != mirr[idx + i + 1]:
                 break
-    return ret
-
-p2_map = {'#': '.', '.': '#'}
-def p2(inp):
-    score = 0
-    for grid in inp:
-        score += check_many(grid)
-
-    return score
-
-def pp(inp):
-    for line in inp:
-        print(line)
-
-def check_many(grid):
-    init_idx = find_mirror_idx(grid)
-    init_idx_t = find_mirror_idx(transpose(grid))
+    return rets
 
 
-    for y in range(len(grid)):
-        for x in range(len(grid[y])):
-            grid[y][x] = p2_map[grid[y][x]]
-            #pp(grid)
-            #print()
-            idx = find_mirror_idx(grid)
-            idx_t = find_mirror_idx(transpose(grid))
+def p2_mirror_score(mirr):
+    init_idxs = set(find_mirror_idx(mirr))
+    init_idxs_t = set(find_mirror_idx(transpose(mirr)))
 
-            if idx and idx != init_idx:
-                #print("normal", init_idx, init_idx_t, idx, idx_t)
-                return idx*100
-            if idx_t and idx_t != init_idx_t:
-                #print("transpose", init_idx, init_idx_t, idx, idx_t)
+    for y in range(len(mirr)):
+        for x in range(len(mirr[y])):
+            mirr[y][x] = p2_map[mirr[y][x]]
+            idx = set(find_mirror_idx(mirr))
+            idx_t = set(find_mirror_idx(transpose(mirr)))
+
+            for idx in idx - init_idxs:
+                return idx * 100
+            for idx_t in idx_t - init_idxs_t:
                 return idx_t
-            grid[y][x] = p2_map[grid[y][x]]
-
-            """ 
-            idx = find_mirror_idx(grid)
-            if idx != init_idx and not t:
-                return 100*idx    
-            if not idx:
-                return find_mirror_idx(transpose(grid))
-            """
-        
+            mirr[y][x] = p2_map[mirr[y][x]]
     return 0
 
 def p1(inp):
     score = 0
-    for grid in inp:
-        cur = 100*find_mirror_idx(grid)
-        if not cur:
-            cur += find_mirror_idx(transpose(grid))
-        score+=cur
+    for mirr in inp:
+        for v in find_mirror_idx(mirr):
+            score += 100 * v
+        for v in find_mirror_idx(transpose(mirr)):
+            score += v
     return score
 
-# 33520 34824
+def p2(inp):
+    score = 0
+    for mirr in inp:
+        score += p2_mirror_score(mirr)
+    return score
+
+inp = [
+    [[char for char in row] for row in pattern.split("\n")]
+    for pattern in open("inp").read().split("\n\n")
+]
+p2_map = {"#": ".", ".": "#"}
 
 print(p1(inp), p2(inp))
+# 33520 34824
