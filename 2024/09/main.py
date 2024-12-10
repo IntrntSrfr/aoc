398e743b9c86825fbf274fb3df7267bfaf050d09
IntrntSrfr <aleksander@tosbakken.com>
2024-12-10 13:44:48 +0100

    9 p1

--- 2024/09/main.py
+++ 2024/09/main.py
@@ -7,27 +7,12 @@ for i, v in enumerate(inp):
     #lol.append f'{(i//2)}'*int(v) if i%2==0 else '*'*int(v)
 
 def p1(inp: list[int]):
-    #print(inp)
-    inp1 = inp.copy()
-    inp2 = []
-    try:
-        while -1 in inp:
-            inp1 = inp.copy()
-            #print(inp)
-            x = inp.pop()
-            while x == -1:
-                x = inp.pop()
-            # find first -1
-            i = inp.index(-1)
-            inp[i] = x
-            #print(inp)
-            inp2 = inp.copy()
-        #print(inp)
-    except Exception:
-        print(inp1)
-        print(inp2)
-        
-    
+    while -1 in inp:
+        x = inp.pop()
+        i = inp.index(-1)
+        inp[i] = x
+        while inp[-1] == -1:
+            inp.pop()
     print(sum(a*b for a, b in enumerate(inp)))
 
 if __name__ == '__main__':
